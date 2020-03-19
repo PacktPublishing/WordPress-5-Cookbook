49a9399a976ea3401f3bbc0abc390c7f08d37bdc
Rakhitha Nimesh <nimeshrmr@gmail.com>
2020-03-19 14:01:33 +0530

    Update Chapter 7

--- Chapter 7/wpcookbookchapter7/archive_books.php
+++ Chapter 7/wpcookbookchapter7/archive_books.php
@@ -1,30 +1,37 @@
 <?php
 get_header();
 ?>
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-		<?php if ( have_posts() ) : ?>
-			<header class="page-header">
-				<?php echo __('Book List','wpccp_ch7'); ?>
-			</header>
-			
-			<?php
-			while ( have_posts() ) : the_post();
-				?>
-				<div style="margin:30px auto;width:80%;background: #eee;padding: 10px"> 
-					<h2 ><?php the_title(); ?></h2>
-					<div><?php the_content(); ?></div>
-					<div>Price : <?php echo get_post_meta(get_the_ID(), 'wpccp_book_price' , true); ?></div>
-					<div>Pages : <?php echo get_post_meta(get_the_ID(), 'wpccp_book_pages' , true); ?></div>
-				</div>
-				<?php
-			endwhile;
-			twentynineteen_the_posts_navigation();
-		else :
-			get_template_part( 'template-parts/content/content', 'none' );
-		endif;
-		?>
-		</main><!-- #main -->
-	</section><!-- #primary -->
+
+ <section id="primary" class="content-area">
+ <main id="main" class="site-main"> 
+ <!-- Step 4 code should be placed in the next line -->
+	 <?php 
+	 if ( have_posts() ) : ?>
+	 <header class="page-header">
+	 <?php echo __('Book List','wpccp_ch7'); ?>
+	 </header>
+	 
+	 <?php
+	 while ( have_posts() ) : the_post();
+	 ?>
+	 	<div style="margin:30px auto;width:80%;background: #eee;padding: 10px"> 
+	 	<h2 ><?php the_title(); ?></h2>
+	 	<div><?php the_content(); ?></div>
+
+	 	<div>Price : <?php echo get_post_meta(get_the_ID(), 'wpccp_book_price' , true); ?></div>
+		<div>Pages : <?php echo get_post_meta(get_the_ID(), 'wpccp_book_pages' , true); ?></div>
+	 	</div>
+
+	 <?php
+	 endwhile;
+	 twentynineteen_the_posts_navigation();
+
+	 else :
+	 get_template_part( 'template-parts/content/content', 'none' );
+	 endif;
+	?>
+ </main><!-- #main -->
+ </section><!-- #primary -->
+
 <?php
 get_footer();
--- Chapter 7/wpcookbookchapter7/user_profile.php
+++ Chapter 7/wpcookbookchapter7/user_profile.php
@@ -1,19 +1,20 @@
 <?php
 get_header();
-
 global $wpcpp_user_id;
 
 $user_info = get_userdata($wpcpp_user_id);
 $full_name = get_user_meta($wpcpp_user_id, 'first_name', true) . " " . get_user_meta($wpcpp_user_id, 'last_name', true);
 ?>
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" style="width: 50%;margin:auto;">
-			<table>
-				<tr><td><?php _e('Username','wpccp_ch7'); ?></td><td><?php echo $user_info->user_login; ?></td></tr>
-				<tr><td><?php _e('Email','wpccp_ch7'); ?></td><td><?php echo $user_info->user_email; ?></td></tr>
-				<tr><td><?php _e('Full Name','wpccp_ch7'); ?></td><td><?php echo $full_name; ?></td></tr>
-			</table>
-		</main><!-- #main -->
-	</section><!-- #primary -->
+
+ <section id="primary" class="content-area">
+ <main id="main" class="site-main" style="width: 50%;margin:auto;">
+ <table>
+ <tr><td><?php _e('Username','wpccp_ch7'); ?></td><td><?php echo $user_info->user_login; ?></td></tr>
+ <tr><td><?php _e('Email','wpccp_ch7'); ?></td><td><?php echo $user_info->user_email; ?></td></tr>
+ <tr><td><?php _e('Full Name','wpccp_ch7'); ?></td><td><?php echo $full_name; ?></td></tr>
+ </table>
+ </main><!-- #main -->
+ </section><!-- #primary -->
+
 <?php
 get_footer();
