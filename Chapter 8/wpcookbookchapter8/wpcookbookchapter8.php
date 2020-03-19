26e760ad93946b65af403e853a36ec3570b32c46
Rakhitha Nimesh <nimeshrmr@gmail.com>
2020-03-19 14:21:31 +0530

    Update chapter 8

--- Chapter 8/wpcookbookchapter8/wpcookbookchapter8.php
+++ Chapter 8/wpcookbookchapter8/wpcookbookchapter8.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: WPCookbook Chapter 8
  * Plugin URI: 
@@ -14,32 +15,13 @@
  */
 
 function wpccp_ch8_custom_search($query) {
-    if ($query->is_search && !is_admin() ) {
-		if(is_user_logged_in()){ 
-		    $query->set('post_type',array('book','post','page'));
-		}else{
-		    $query->set('post_type',array('post'));
-		}
-
-		// Searching specific category and meta value
-		$query->set( 'cat', '55' );  8
-        $query->set( 'meta_key', 'wpccp_post_allowed_user' );
-        $query->set( 'meta_value', '8' ); 
-
-        // Limit serahcing to set of posts
-        $query->set ( 'post__in', array('1','4','6') ); 
-
-        // Differntiate search results for multiple searches
-        if(isset($_GET['search_type'])) {
-            $type = $_GET['search_type'];
-            if($type == 'book') {
-                $query->set('post_type',array('book'));
-                $query->set( 'cat', '-4' );
-            }
-        }       
-    }
-
-
-	return $query;
+  if ($query->is_search && !is_admin() ) {
+    if(is_user_logged_in()){ 
+      $query->set('post_type',array('book','post','page'));
+    }else{
+      $query->set('post_type',array('post'));
+    } 
+  }
+  return $query;
 }
 add_filter('pre_get_posts','wpccp_ch8_custom_search');
